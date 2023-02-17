64fc04c6f5f22080ddba0547261835a1257ac185
staledonuts <goblinburrow@gmail.com>
2023-02-17 11:23:29 +0100

    Angy

---
+++ Rattle of Bones/start.sh
@@ -0,0 +1,3 @@
+dotnet build "/home/staledonuts/Dokument/Projects/Rattle_of_Bones_Raylib/Rattle of Bones/Rattle of Bones.csproj" /property:GenerateFullPaths=true /consoleloggerparameters:NoSummary
+export MESA_GL_VERSION_OVERRIDE=3.3
+export MESA_GLSL_VERSION_OVERRIDE=330
