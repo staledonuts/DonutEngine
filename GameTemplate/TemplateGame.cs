135ea04e7357420b7a57322158afb8bccc4c6cf3
staledonut <goblinburrow@gmail.com>
2023-11-14 07:08:34 +0100

    Mysystem tutorial

--- GameTemplate/TemplateGame.cs
+++ GameTemplate/TemplateGame.cs
@@ -5,14 +5,21 @@ using Template.Scenes;
 
 namespace Template;
 
+/// <summary>
+/// The TemplateGame class inherits from "Game" which has the commandline argument parser and is set up to pre initialize all the base systems.
+/// </summary>
 public class TemplateGame : Game
 {
+    /// <param name="gamename"> Here you insert the Window Name </param>
     public TemplateGame(string gamename) : base(gamename){}
     public override void InitGame(string[] args)
     {
         base.InitGame(args);
     }
 
+    /// <summary>
+    /// SetupGame() is where you create and add your Custom SystemClass'es to the EngineSystens static class. SystemClass can be found under the Engine.Systems namespace and inherited.
+    /// </summary>
     public override void SetupGame()
     {
         EngineSystems.GetSystem<SceneManager>().AddScene(2, new Gameplay());
---
+++ GameTemplate/src/Systems/MySystem.cs
@@ -0,0 +1,31 @@
+using Engine.Systems;
+
+namespace Template;
+
+class MySystem : SystemClass
+{
+    public override void DrawUpdate()
+    {
+        
+    }
+
+    public override void Initialize()
+    {
+        
+    }
+
+    public override void LateUpdate()
+    {
+        
+    }
+
+    public override void Shutdown()
+    {
+        
+    }
+
+    public override void Update()
+    {
+        
+    }
+}
